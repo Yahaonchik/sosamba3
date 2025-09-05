3bc07c9e0767f3735cc6b3b548a83708f569f7a9
Yahaonchik <Danilqa123456@gmail.com>
2025-09-05 14:46:55 +0300

    Update SEOHead.js

--- components/SEOHead.js
+++ components/SEOHead.js
@@ -34,7 +34,7 @@ const SEOHead = ({
 
   // Определяем robots: из пропса, из seoConfig по текущему пути, иначе индексируем
   const routeSEO = getSEOData(router.pathname)
-  const finalRobots = robots || (routeSEO && routeSEO.robots) || 'noindex, follow'
+  const finalRobots = robots || (routeSEO && routeSEO.robots) || 'index, follow'
 
   // Проверка наличия типа в переданных структурированных данных (объект или массив)
   const hasType = (data, type) => {
